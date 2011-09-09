ec27757883846204de1abf592ced0a146319354f
Josef Šimánek <retro@ballgag.cz>
2011-09-09 21:19:41 +0200

    Let's add new syntax

--- lib/refinery/reference_link/referencer.rb
+++ lib/refinery/reference_link/referencer.rb
@@ -8,7 +8,7 @@ module Refinery
           objects = []
           doc = ::Nokogiri::HTML(text)
           doc.css('a').each do |link|
-            if link['href'] and link['href'].match(%r{^[^:\/]*$})
+            if link['href'] and !link['href'].match(%r{^http:\/\/.+\..+$}) # all props to jschoolcraft
               objects << Refinery::ReferenceLink::Reference.new(:title => link['href'], :html => link)
             end
           end
